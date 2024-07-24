18c94480982248245760f2534dba22c39f90ef7b
Erkowaa <132221816+Erkowaa@users.noreply.github.com>
2024-07-24 20:35:37 +0500

    Add files via upload

---
+++ contacty1main/contacty/ContactsInfo.swift
@@ -0,0 +1,16 @@
+//
+//  ContactsInfo.swift
+//  task
+//
+//  Created by Yergeldi.
+//
+
+import Foundation
+
+struct ContactsInfo: Codable{
+    var name = ""
+    var number = ""
+    var isCalled = false
+
+    //типа совершил ли звонок пользователь
+}
